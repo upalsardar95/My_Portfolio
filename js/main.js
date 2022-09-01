fa09024785d7424cc935d6af181a046d5a11e8d0
upalsardar95 <upal.sardar@gmail.com>
2022-09-01 13:43:53 +0530

    Code Update 01/09/2022

--- js/main.js
+++ js/main.js
@@ -4,9 +4,68 @@ window.addEventListener("scroll", () => {
   header.classList.toggle("sticky", window.scrollY > 0);
 });
 
+//Services section - Modal
+const serviceModals = document.querySelectorAll(".service-modal");
+const learnMoreBtns = document.querySelectorAll(".learn-more-btn");
+const modalCloseBtns = document.querySelectorAll(".modal-close-btn");
+
+var modal = (modalClick) => {
+  serviceModals[modalClick].classList.add("active");
+};
+
+learnMoreBtns.forEach((learnMoreBtn, i) => {
+  learnMoreBtn.addEventListener("click", () => {
+    modal(i);
+  });
+});
+
+modalCloseBtns.forEach((modalCloseBtn) => {
+  modalCloseBtn.addEventListener("click", () => {
+    serviceModals.forEach((modalView) => {
+      modalView.classList.remove("active");
+    });
+  });
+});
+
 //Portfolio section - Modal
 
+const portfolioModals = document.querySelectorAll(".portfolio-model");
+const imgCards = document.querySelectorAll(".img-card");
+const portfolioCloseBtns = document.querySelectorAll(".portfolio-close-btn");
+
+var portfolioModal = (modalClick) => {
+  portfolioModals[modalClick].classList.add("active");
+};
+
+imgCards.forEach((imgCard, i) => {
+  imgCard.addEventListener("click", () => {
+    portfolioModal(i);
+  });
+});
+
+portfolioCloseBtns.forEach((portfolioCloseBtn) => {
+  portfolioCloseBtn.addEventListener("click", () => {
+    portfolioModals.forEach((portfolioModalView) => {
+      portfolioModalView.classList.remove("active");
+    });
+  });
+});
+
 //Our clients - Swiper
+
+var swiper = new Swiper(".client-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
 //Website dark/light theme
 
