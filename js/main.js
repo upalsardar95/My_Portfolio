4bc89749b8d2497d3520c5b472bfc236fedcf56e
upalsardar95 <upal.sardar@gmail.com>
2022-09-04 17:46:12 +0530

    Update 04/09/2022

--- js/main.js
+++ js/main.js
@@ -69,9 +69,67 @@ var swiper = new Swiper(".client-swiper", {
 
 //Website dark/light theme
 
+const themeBtn = document.querySelector(".theme-btn");
+
+themeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme");
+  themeBtn.classList.toggle("sun");
+
+  localStorage.setItem("saved-theme", getCurrentTheme());
+  localStorage.setItem("saved-icon", getCurrentIcon());
+});
+
+const getCurrentTheme = () =>
+  document.body.classList.contains("dark-theme") ? "dark" : "light";
+
+const getCurrentIcon = () =>
+  themeBtn.classList.contains("sun") ? "sun" : "moon";
+
+const savedTheme = localStorage.getItem("saved-theme");
+const savedIcon = localStorage.getItem("saved-icon");
+
+if (savedTheme) {
+  document.body.classList[savedTheme === "dark" ? "add" : "remove"](
+    "dark-theme"
+  );
+
+  themeBtn.classList[savedIcon === "sun" ? "add" : "remove"]("sun");
+}
+
 //Scroll to top button
 
+const scrollTopBtn = document.querySelector(".scrollToTop-btn");
+window.addEventListener("scroll", () => {
+  scrollTopBtn.classList.toggle("active", window.scrollY > 500);
+});
+
+scrollTopBtn.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
+
 //Navigation menu items active on page scroll
+
+window.addEventListener("scroll", () => {
+  const sections = document.querySelectorAll("section");
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    let sectionHeight = current.offsetHeight;
+    let sectionTop = current.offsetTop - 50;
+    let id = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav-items a[href*=" + id + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector(".nav-items a[href*=" + id + "]")
+        .classList.remove("active");
+    }
+  });
+});
 
 //Responsive navigation menu toggle
 
